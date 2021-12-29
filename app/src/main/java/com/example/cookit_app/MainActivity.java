77943553c24044e3c4b304f38d3c61e072e18909
artur810 <j3123475@gmail.com>
2021-12-29 18:17:20 +0200

    add ratingBar

--- app/src/main/java/com/example/cookit_app/MainActivity.java
+++ app/src/main/java/com/example/cookit_app/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.cookit_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RatingBar stars;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stars = findViewById(R.id.rating);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            rateFragment dialogFragment = new rateFragment();
+            dialogFragment.show(getSupportFragmentManager(), "dialog fragment");
+        });
     }
 }
---
+++ app/src/main/java/com/example/cookit_app/rateFragment.java
@@ -0,0 +1,27 @@
+package com.example.cookit_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class rateFragment extends DialogFragment {
+
+    private RatingBar stars;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.rate_fragment, container, false);
+
+        stars = view.findViewById(R.id.rating);
+
+        return view;
+    }
+}
