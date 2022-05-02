083b6625bbbe8f785a340f760ee1e7958bea7508
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2022-05-02 13:07:06 +0300

    user retrofit requests are all set up

--- app/src/main/java/com/example/cookit_app/server/RetrofitInterface.java
+++ app/src/main/java/com/example/cookit_app/server/RetrofitInterface.java
@@ -1,5 +1,30 @@
 package com.example.cookit_app.server;
 
+import com.example.cookit_app.server.scheme.Recipe;
+import com.example.cookit_app.server.scheme.User;
+import java.util.HashMap;
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+
 public interface RetrofitInterface {
 
+    @POST("/users/new-user")
+    Call<Void> newUser(@QueryMap HashMap<String, String> userData);
+
+    @PUT("/users/update-user")
+    Call<Void> updateUser(@Query("username") String username, @QueryMap HashMap<String, String> newDAta);
+
+    @DELETE("/users/delete-user")
+    Call<Void> deleteUser(@Query("username") String username); // delete his recipes as well
+
+    @GET("/users/get-user")
+    Call<User> getUSer(@Query("username") String username);
+
+    @POST("/users/log-in-user")
+    Call<Void> logInUser(@Query("username") String username, @Query("password") String password);
 }
---
+++ app/src/main/java/com/example/cookit_app/server/scheme/Recipe.java
@@ -0,0 +1,46 @@
+package com.example.cookit_app.server.scheme;
+
+import java.util.List;
+
+public class Recipe {
+
+    private String _id, author_username, recipe_name, description, image;
+    private List<String> mealtime, tags;
+    private Number average_rate, rates_number;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getAuthor_username() {
+        return author_username;
+    }
+
+    public String getRecipe_name() {
+        return recipe_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public List<String> getMealtime() {
+        return mealtime;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public Number getAverage_rate() {
+        return average_rate;
+    }
+
+    public Number getRates_number() {
+        return rates_number;
+    }
+}
