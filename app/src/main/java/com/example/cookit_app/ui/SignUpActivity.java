c660116727f7c654fe127579c3979f9e9a703f73
artur810 <j3123475@gmail.com>
2022-05-03 23:31:33 +0300

    add check to field in signup activity

--- app/src/main/java/com/example/cookit_app/ui/SignUpActivity.java
+++ app/src/main/java/com/example/cookit_app/ui/SignUpActivity.java
@@ -3,9 +3,12 @@ package com.example.cookit_app.ui;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -29,26 +32,66 @@ import retrofit2.Retrofit;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText name_et, email_et, username_et, password_et, confirm_password_et;
+    Button submit_signup;
+
     @RequiresApi(api = Build.VERSION_CODES.O) @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        EditText name_et = findViewById(R.id.name);
-        EditText email_et = findViewById(R.id.email);
-        EditText username_et = findViewById(R.id.username);
-        EditText password_et = findViewById(R.id.password);
-        EditText confirm_password_et = findViewById(R.id.confirm_password);
+        name_et = findViewById(R.id.name);
+        email_et = findViewById(R.id.email);
+        username_et = findViewById(R.id.username);
+        password_et = findViewById(R.id.password);
+        confirm_password_et = findViewById(R.id.confirm_password);
+        submit_signup = findViewById(R.id.submit_signup);
 
         //TODO: make sure data is not empty, password == confirm_password and email with good format.
 
-        String name = name_et.getText().toString();
-        String email = email_et.getText().toString();
-        String username = username_et.getText().toString();
-        String password = password_et.getText().toString();
+        submit_signup.setOnClickListener(view -> {
 
-//        try {
-            //RSA encryption username, password and email
+            String name = name_et.getText().toString();
+            String email = email_et.getText().toString();
+            String username = username_et.getText().toString();
+            String password = password_et.getText().toString();
+            String confirmPassword = confirm_password_et.getText().toString();
+            Boolean correct = true;
+
+            if(name_et.getText().toString().trim().isEmpty()){
+                name_et.setError("Field is empty");
+                name_et.requestFocus();
+                correct = false;
+            }
+
+            if(!validateEmailAddress(email_et)){
+                correct = false;
+            }
+            if(username_et.getText().toString().trim().isEmpty()){
+                username_et.setError("Field is empty");
+                username_et.requestFocus();
+                correct = false;
+            }
+            if(password_et.getText().toString().trim().isEmpty()){
+                password_et.setError("Field is empty");
+                password_et.requestFocus();
+                correct = false;
+            }
+            if(confirm_password_et.getText().toString().trim().isEmpty()){
+                confirm_password_et.setError("Field is empty");
+                confirm_password_et.requestFocus();
+                correct = false;
+            }else if (!confirm_password_et.getText().toString().equals(password_et.getText().toString())){
+                confirm_password_et.setError("Incompatible passwords");
+                correct = false;
+            }
+
+            if(correct){
+
+                if(!username.isEmpty() && !password.isEmpty()){
+
+                    //        try {
+                    //RSA encryption username, password and email
 //            String encryptedUsername = Base64.getEncoder().encodeToString(RSA.encrypt(username, /*TODO ADD A REAL KEY*/"publicKey"));
 //            Log.d("tesTag", encryptedUsername);
 //            String encryptedPassword = Base64.getEncoder().encodeToString(RSA.encrypt(password, /*TODO ADD A REAL KEY*/"publicKey"));
@@ -56,34 +99,59 @@ public class SignUpActivity extends AppCompatActivity {
 //            String encryptedEmail = Base64.getEncoder().encodeToString(RSA.encrypt(email, /*TODO ADD A REAL KEY*/"publicKey"));
 //            Log.d("tesTag", encryptedEmail);
 
-            //sending the request
-            HashMap<String, String> userData = new HashMap<>();
+                    //sending the request
+                    HashMap<String, String> userData = new HashMap<>();
 
-            userData.put("name", name);
-            userData.put("email", email);
-            userData.put("username", username);
-            userData.put("password", password);
+                    userData.put("name", name);
+                    userData.put("email", email);
+                    userData.put("username", username);
+                    userData.put("password", password);
 
-            Call<Void> call = new Retrofit2Init().retrofitInterface.newUser(userData);
+                    Call<Void> call = new Retrofit2Init().retrofitInterface.newUser(userData);
 
-            call.enqueue(new Callback<Void>() {
-                @Override
-                public void onResponse(Call<Void> call, Response<Void> response) {
-                    if(response.isSuccessful()){
-                        Toast.makeText(getBaseContext(), "Signed up successfully.", Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(getBaseContext(),response.code() + "\n" + response.message(), Toast.LENGTH_LONG).show();
-                    }
-                }
+                    call.enqueue(new Callback<Void>() {
+                        @Override
+                        public void onResponse(Call<Void> call, Response<Void> response) {
+                            if(response.isSuccessful()){
+                                Toast.makeText(getBaseContext(), "Signed up successfully.", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(getBaseContext(),response.code() + "\n" + response.message(), Toast.LENGTH_LONG).show();
+                            }
+                        }
 
-                @Override
-                public void onFailure(Call<Void> call, Throwable t) {
-                    Toast.makeText(getBaseContext(), t.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            });
+                        @Override
+                        public void onFailure(Call<Void> call, Throwable t) {
+                            Toast.makeText(getBaseContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
 
 //        } catch (BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException | InvalidKeyException | NoSuchAlgorithmException e) {
 //            e.printStackTrace();
 //        }
+
+                }
+
+            }
+
+        });
+
+    }
+
+    private boolean validateEmailAddress(EditText email){
+        String emailInput = email.getText().toString();
+
+        if(emailInput.isEmpty()){
+            email_et.setError("Field is empty");
+            email_et.requestFocus();
+            return false;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()){
+            email_et.setError("email isn't correct");
+            email_et.requestFocus();
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
