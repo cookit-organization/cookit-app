efb09a292f10aba0d129465e3d34e275ccbe9a32
artur810 <j3123475@gmail.com>
2022-05-16 23:24:46 +0300

    add.java added spinner with checkbox list

--- app/src/main/java/com/example/cookit_app/ui/fragments/homepage_fragmets/Add.java
+++ app/src/main/java/com/example/cookit_app/ui/fragments/homepage_fragmets/Add.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -12,10 +14,14 @@ import android.widget.Spinner;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.cookit_app.R;
+import com.example.cookit_app.ui.MultiSpinner;
 
-public class Add extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    Spinner tags, meal_time;
+public class Add extends Fragment{
+
+    MultiSpinner tags, meal_time;
 
     private final int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
 
@@ -25,6 +31,22 @@ public class Add extends Fragment {
 
         tags = view.findViewById(R.id.tags);
         meal_time = view.findViewById(R.id.meal_time);
+
+        List<String> listTags = new ArrayList<>();
+
+        listTags.add("meat");
+        listTags.add("soup");
+        listTags.add("fish");
+
+        tags.setItems(listTags, "check tags", selected -> {});
+
+        List<String> listMealTime = new ArrayList<>();
+
+        listMealTime.add("Morning");
+        listMealTime.add("Afternoon");
+        listMealTime.add("Night");
+
+        meal_time.setItems(listMealTime, "check meal time", selected -> {});
 
         // if pressed on add button check if component is not empty if not take the values and then do this again
         // if pressed on remove button remove current card
